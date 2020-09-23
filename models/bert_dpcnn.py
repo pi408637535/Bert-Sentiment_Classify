453e6366ad1dad9c7760a17244fec2e2aa63a129
piguanghua <piguanghua@163.com>
2020-09-23 21:54:04 +0800

    增加dpcnn

---
+++ models/bert_dpcnn.py
@@ -0,0 +1,98 @@
+# coding: UTF-8
+import torch
+import torch.nn as nn
+# from pytorch_pretrained_bert import BertModel, BertTokenizer
+from transformers import BertModel, BertTokenizer
+import torch.nn.functional as F
+
+class Config(object):
+
+    """配置参数"""
+    def __init__(self, dataset):
+        self.model_name = 'bert'
+        self.train_path = "data/" + dataset + '/train.csv'
+        self.dev_path = "data/" + dataset + '/dev.csv'   # 训练集
+        #self.dev_path = dataset + '/data/dev.txt'                                    # 验证集
+        #self.test_path = dataset + '/data/test.txt'                                  # 测试集
+        #self.class_list = [x.strip() for x in open( dataset + '/data/class.txt').readlines()]                                # 类别名单
+
+        self.class_list = [0,1,2]
+
+        self.save_path = "data/" + dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")   # 设备
+        self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
+        self.num_classes = len(self.class_list)                         # 类别数
+        self.num_epochs = 3                                             # epoch数
+        self.pad_size = 512                                              # 每句话处理成的长度(短填长切)
+        self.learning_rate = 5e-5                                       # 学习率
+        self.bert_learning_rate = 5e-5
+        self.other_learning_rate = 1e-3
+
+        self.bert_path = './bert_pretrain'
+        self.datasetpkl = "pkl/data.pkl"
+        self.tokenizer = BertTokenizer.from_pretrained(self.bert_path + "/bert-base-chinese-vocab.txt")
+        self.lstm_hidden = 512
+        self.bert_hidden = 768
+        self.weight_decay = 0
+        self.num_filters = 250
+
+
+class Model(nn.Module):
+
+    def __init__(self, config):
+        super(Model, self).__init__()
+        self.bert = BertModel.from_pretrained(config.bert_path)
+        for param in self.bert.parameters():
+            param.requires_grad = True
+
+        self.conv_region = nn.Conv2d(1, config.num_filters, (3, config.bert_hidden), stride=1)
+        self.conv = nn.Conv2d(config.num_filters, config.num_filters, (3, 1), stride=1)
+        self.max_pool = nn.MaxPool2d(kernel_size=(3, 1), stride=2)
+        self.padding1 = nn.ZeroPad2d((0, 0, 1, 1))  # top bottom
+        self.padding2 = nn.ZeroPad2d((0, 0, 0, 1))  # bottom
+        self.relu = nn.ReLU()
+        self.fc = nn.Linear(config.num_filters, config.num_classes)
+
+    def forward(self, x):
+        input_ids = x[0] #batch,split,seq
+        input_mask = x[1]
+        segment_ids = x[2]
+
+        flat_input_ids = input_ids.view(-1, input_ids.size(-1))
+        flat_input_mask = input_mask.view(-1, input_ids.size(-1))
+        flat_segment_ids = segment_ids.view(-1, input_ids.size(-1))
+
+        #_, pooled = self.bert(flat_input_ids, attention_mask=flat_input_mask, token_type_ids= flat_segment_ids,output_hidden_states=False)
+        _, pooled = self.bert(input_ids=flat_input_ids, token_type_ids=flat_segment_ids,
+                  attention_mask=flat_input_mask)
+
+        batch,split_num = input_ids.shape[0],input_ids.shape[1]
+
+        output = pooled.reshape(batch,split_num,-1)
+
+        x = output.unsqueeze(1)  # [batch_size, 1, seq_len, embed]
+        x = self.conv_region(x)  # [batch_size, 250, seq_len-3+1, 1]
+
+        x = self.padding1(x)  # [batch_size, 250, seq_len, 1]
+        x = self.relu(x)
+        x = self.conv(x)  # [batch_size, 250, seq_len-3+1, 1]
+        x = self.padding1(x)  # [batch_size, 250, seq_len, 1]
+        x = self.relu(x)
+        x = self.conv(x)  # [batch_size, 250, seq_len-3+1, 1]
+        while x.size()[2] > 2:
+            x = self._block(x)
+        x = x.squeeze()  # [batch_size, num_filters(250)]
+        x = self.fc(x)
+        return x
+
+    def _block(self, x):
+        x = self.padding2(x)
+        px = self.max_pool(x)
+        x = self.padding1(px)
+        x = F.relu(x)
+        x = self.conv(x)
+        x = self.padding1(x)
+        x = F.relu(x)
+        x = self.conv(x)
+        x = x + px  # short cut
+        return x
--- train_eval.py
+++ train_eval.py
@@ -38,19 +38,19 @@ def train(config, model, train_iter, dev_iter, test_iter):
     #no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
 
     #LayerNorm,bias是不需要decay的
-    # optimizer_grouped_parameters = [
-    #     {'params': [p for n, p in param_optimizer if "bert" in n], "lr":config.bert_learning_rate, 'weight_decay': 0.01 },
-    #     {'params': [p for n, p in param_optimizer if "bert" not in n], "lr": config.other_learning_rate,'weight_decay': 0.01 },
-    #
-    # ]
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if "bert" in n], "lr":config.bert_learning_rate, 'weight_decay': 0.01 },
+        {'params': [p for n, p in param_optimizer if "bert" not in n], "lr": config.other_learning_rate,'weight_decay': 0.01 },
+
+    ]
 
     # LayerNorm,bias是不需要decay的
-    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-    optimizer_grouped_parameters = [
-        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
-         'weight_decay': config.weight_decay},
-        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-    ]
+    # no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+    # optimizer_grouped_parameters = [
+    #     {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+    #      'weight_decay': config.weight_decay},
+    #     {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    # ]
 
     #optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
     #optimizer = BertAdam(optimizer_grouped_parameters, lr=config.learning_rate,  warmup=0.05,t_total=len(train_iter) * config.num_epochs)
